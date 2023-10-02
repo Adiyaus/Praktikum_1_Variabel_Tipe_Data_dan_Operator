2770aa4e9c31a43e6b04e802f4e8765c0ef10dce
Adiyaus <145339274+Adiyaus@users.noreply.github.com>
2023-10-02 17:35:37 +0800

    Update PRAK104_2310817210022_MUHAMMAD_ADH-DHIYA'US_SALIM.py

--- PRAK104_2310817210022_MUHAMMAD_ADH-DHIYA'US_SALIM.py
+++ PRAK104_2310817210022_MUHAMMAD_ADH-DHIYA'US_SALIM.py
@@ -1,13 +1,13 @@
 Sepatu_A = 400000
 Sepatu_B = 350000
 
-diskon_A = 0.13
-diskon_B = 0.21
+diskon_A = 0.21
+diskon_B = 0.13
 
 harga_diskonA = Sepatu_A - (Sepatu_A * diskon_A)
 harga_diskonB = Sepatu_B - (Sepatu_B *  diskon_B)
 
-print("Sepatu_A", Sepatu_A)
-print("Sepatu_B", Sepatu_B)
-print("harga_diskonA", harga_diskonA)
-print("harga_diskonB", harga_diskonB)
+print("Harga Sepatu A:", Sepatu_A)
+print("Harga Sepatu B:", Sepatu_B)
+print("Harga Sepatu A setelah diskon:", harga_diskonA)
+print("Harga Sepatu A setelah diskon:", harga_diskonB)
